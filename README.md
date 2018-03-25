2a19e161c7166aa108073990c2a760608d247ed4
Jay Lux Ferro <jayluxferro@sperixlabs.org>
2018-03-25 20:52:54 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# WiliLeaks Vault7 Files Download
+This project uses python and axel downloader to download all Vault7 documents on wikileaks.org. The project has only been tested on linux.
+## Instructions
+1. Download axel
+```sudo apt-get install axel -y```
+2. Download python requirements using pip
+```
+sudo -H pip install -r requirements.txt
+```
+3. Run the script to download all the files
+```
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+from bs4 import BeautifulSoup
+import urllib2
+import subprocess
+import threading
+import sys
+
+# default definitions
+base_url = "https://wikileaks.org/vault7/document/"
+
+def download_file(target_url, filname):
+	print("[+] Downloading: " + str(file_name))
+        subprocess.call(['wget', target_url, '-O', file_name])
+
+# getting web page
+web_data = urllib2.urlopen(base_url)
+web_data = "".join(web_data.readlines())
+
+# retrieving links
+all_links = []
+counter = 0
+links = BeautifulSoup(web_data, "lxml")
+for link in links.find_all('a'):
+	current_link = link.get('href')
+	# performing match
+	if str(current_link[3:11]) == "document":
+		if counter == 0:
+			pass
+		else:
+			all_links.append(current_link)
+			print("[+] Link found: " + str(current_link))
+		counter += 1
+
+download_urls = []
+for link in all_links:
+	folder_name = link[12:]
+	download_page = urllib2.urlopen(base_url + link)
+	page_content = "".join(download_page.readlines())
+	download_links = BeautifulSoup(page_content, "lxml")
+	for dl in download_links.find_all('a'):
+		download_link = dl.get('href')
+		dl = str(download_link)
+		if dl[len(dl)-3:len(dl)] == "pdf":
+			download_urls.append(base_url + link + download_link)
+			print("[+] Download link found: " + str(dl))
+			
+		
+for x in range(len(download_urls)):
+	file_name = str(download_urls[x]).split("/")[-1]
+	trigger = threading.Thread(target=download_file, args=(download_urls[x], file_name))
+	trigger.start()
+
+"""
+for urls in download_urls:
+	# getting filename
+	file_name = urls.split("/")[-1]
+	print("[+] Downloading: " + str(file_name))
+	subprocess.call(['wget', urls, '-O', file_name])
+"""
