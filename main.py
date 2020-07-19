8a62c2a2e7542d9d1778699fdf26d5fa4f68cda8
Jay Lux Ferro <jayluxferro@sperixlabs.org>
2020-07-19 21:12:22 +0000

    updated codebase

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import urllib2
 import subprocess
 import threading
 import sys
+from time import sleep
 
 # default definitions
 base_url = "https://wikileaks.org/vault7/document/"
@@ -13,11 +14,24 @@ def download_file(target_url, filname):
 	print("[+] Downloading: " + str(file_name))
         subprocess.call(['wget', target_url, '-O', file_name])
 
+def searchForDownloadLinks(link):
+	folder_name = link[12:]
+	download_page = urllib2.urlopen(base_url + link)
+	page_content = "".join(download_page.readlines())
+	download_links = BeautifulSoup(page_content, "lxml")
+	for dl in download_links.find_all('a'):
+		download_link = dl.get('href')
+		dl = str(download_link)
+		if dl[len(dl)-3:len(dl)] == "pdf":
+			download_urls.append(base_url + link + download_link)
+			print("[+] Download link found: " + str(dl))
+
 # getting web page
 web_data = urllib2.urlopen(base_url)
 web_data = "".join(web_data.readlines())
 
 # retrieving links
+download_urls = []
 all_links = []
 counter = 0
 links = BeautifulSoup(web_data, "lxml")
@@ -32,29 +46,22 @@ for link in links.find_all('a'):
 			print("[+] Link found: " + str(current_link))
 		counter += 1
 
-download_urls = []
+threads = []
+
 for link in all_links:
-	folder_name = link[12:]
-	download_page = urllib2.urlopen(base_url + link)
-	page_content = "".join(download_page.readlines())
-	download_links = BeautifulSoup(page_content, "lxml")
-	for dl in download_links.find_all('a'):
-		download_link = dl.get('href')
-		dl = str(download_link)
-		if dl[len(dl)-3:len(dl)] == "pdf":
-			download_urls.append(base_url + link + download_link)
-			print("[+] Download link found: " + str(dl))
-			
-		
+    t = threading.Thread(target=searchForDownloadLinks, args=(link,))
+    threads.append(t)
+
+# Start all threads
+for x in threads:
+    sleep(.1)
+    x.start()
+
+ # Wait for all of them to finish
+for x in threads:
+    x.join()
+
+
 for x in range(len(download_urls)):
 	file_name = str(download_urls[x]).split("/")[-1]
-	trigger = threading.Thread(target=download_file, args=(download_urls[x], file_name))
-	trigger.start()
-
-"""
-for urls in download_urls:
-	# getting filename
-	file_name = urls.split("/")[-1]
-	print("[+] Downloading: " + str(file_name))
-	subprocess.call(['wget', urls, '-O', file_name])
-"""
+        download_file(download_urls[x], file_name)
