280993c07f28ec1a50bee1a8f7199a91c2b440e9
Jay <jayluxferro@sperixlabs.org>
2023-02-18 05:35:21 +0000

    code formatted

--- main.py
+++ main.py
@@ -11,20 +11,20 @@ from time import sleep
 base_url = "https://wikileaks.org/vault7/document/"
 
 def download_file(target_url, filname):
-	print("[+] Downloading: " + str(file_name))
-        subprocess.call(['wget', target_url, '-O', file_name])
+    print("[+] Downloading: " + str(file_name))
+    subprocess.call(['wget', target_url, '-O', file_name])
 
 def searchForDownloadLinks(link):
-	folder_name = link[12:]
-	download_page = urllib2.urlopen(base_url + link)
-	page_content = "".join(download_page.readlines())
-	download_links = BeautifulSoup(page_content, "lxml")
-	for dl in download_links.find_all('a'):
-		download_link = dl.get('href')
-		dl = str(download_link)
-		if dl[len(dl)-3:len(dl)] == "pdf":
-			download_urls.append(base_url + link + download_link)
-			print("[+] Download link found: " + str(dl))
+    folder_name = link[12:]
+    download_page = urllib2.urlopen(base_url + link)
+    page_content = "".join(download_page.readlines())
+    download_links = BeautifulSoup(page_content, "lxml")
+    for dl in download_links.find_all('a'):
+        download_link = dl.get('href')
+        dl = str(download_link)
+        if dl[len(dl)-3:len(dl)] == "pdf":
+            download_urls.append(base_url + link + download_link)
+            print("[+] Download link found: " + str(dl))
 
 # getting web page
 web_data = urllib2.urlopen(base_url)
@@ -36,15 +36,15 @@ all_links = []
 counter = 0
 links = BeautifulSoup(web_data, "lxml")
 for link in links.find_all('a'):
-	current_link = link.get('href')
-	# performing match
-	if str(current_link[3:11]) == "document":
-		if counter == 0:
-			pass
-		else:
-			all_links.append(current_link)
-			print("[+] Link found: " + str(current_link))
-		counter += 1
+    current_link = link.get('href')
+    # performing match
+    if str(current_link[3:11]) == "document":
+        if counter == 0:
+            pass
+        else:
+            all_links.append(current_link)
+            print("[+] Link found: " + str(current_link))
+        counter += 1
 
 threads = []
 
@@ -57,13 +57,13 @@ for x in threads:
     sleep(.1)
     x.start()
 
- # Wait for all of them to finish
+    # Wait for all of them to finish
 for x in threads:
     x.join()
 
 
 for x in range(len(download_urls)):
-	file_name = str(download_urls[x]).split("/")[-1]
-        download_file(download_urls[x], file_name)
-        # sleep for 5s to prevent server overload
-        sleep(5)
+    file_name = str(download_urls[x]).split("/")[-1]
+    download_file(download_urls[x], file_name)
+    # sleep for 5s to prevent server overload
+    sleep(5)
